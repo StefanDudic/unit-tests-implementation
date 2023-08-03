03e98327d4a4b8af1bfd88ed34b250a18997aac7
Stefan Dudic <v-sdudic@microsoft.com>
2023-08-03 14:39:59 +0200

    Add project files.

---
+++ WiredBrainCoffee.DataProcessor/Processing/MachineDataProcessor.cs
@@ -0,0 +1,63 @@
+﻿using WiredBrainCoffee.DataProcessor.Data;
+using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing
+{
+    public class MachineDataProcessor
+    {
+        private readonly Dictionary<string, int> _countPerCoffeeType = new();
+        private readonly ICoffeeCountStore _coffeeCountStore;
+        private MachineDataItem? _previouseItem;
+
+        public MachineDataProcessor(ICoffeeCountStore coffeeCountStore)
+        {
+            _coffeeCountStore = coffeeCountStore;
+        }
+
+        public void ProcessItems(MachineDataItem[] dataItems)
+        {
+            _previouseItem = null; 
+            _countPerCoffeeType.Clear();
+
+            foreach (var dataItem in dataItems)
+            {
+                ProcessItem(dataItem);
+            }
+
+            SaveCountPerCoffeeType();
+        }
+
+        private void ProcessItem(MachineDataItem dataItem)
+        {
+
+            if (!IsNewerThanPreviouseItem(dataItem))
+            {
+                return;
+            }
+                if (!_countPerCoffeeType.ContainsKey(dataItem.CoffeeType))
+                {
+                    _countPerCoffeeType.Add(dataItem.CoffeeType, 1);
+                }
+                else
+                {
+                    _countPerCoffeeType[dataItem.CoffeeType]++;
+                }
+
+                _previouseItem = dataItem;
+        }
+
+        private bool IsNewerThanPreviouseItem(MachineDataItem dataItem)
+        {
+            return _previouseItem is null
+                || _previouseItem.CreatedAt < dataItem.CreatedAt;
+        }
+
+        private void SaveCountPerCoffeeType()
+        {
+            foreach (var entry in _countPerCoffeeType)
+            {
+               _coffeeCountStore.Save(new CoffeeCountItem(entry.Key, entry.Value));
+            }
+        }
+    }
+}
---
+++ WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.CompilerServices;
+using WiredBrainCoffee.DataProcessor.Data;
+using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing;
+
+public class MachineDataProcessorTests : IDisposable
+{
+    private readonly FakeCoffeeCountStore _coffeeCountStore;
+    private readonly MachineDataProcessor _machineDataProcessor;
+
+    public MachineDataProcessorTests()
+    {
+        _coffeeCountStore = new();
+        _machineDataProcessor = new(_coffeeCountStore);
+    }
+
+    [Fact]
+    public void ShouldSaveCountPerCoffeeType()
+    {
+        // Arrange
+        var items = new[]
+        {
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,0,0)),
+           new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0))
+        };
+
+        // Act
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeeCountStore.SavedItems.Count);
+
+        var item = _coffeeCountStore.SavedItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeeCountStore.SavedItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
+    [Fact]
+    public void ShouldIgnoreItemsThatAreNotNewer()
+    {
+        // Arrange
+        var items = new[]
+        {
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,0,0)), // ignored
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,10,0)), // ignored
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,10,0)), 
+           new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0)),
+           new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0)), // ignored
+        };
+
+        // Act
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeeCountStore.SavedItems.Count);
+
+        var item = _coffeeCountStore.SavedItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeeCountStore.SavedItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
+    [Fact]
+    public void ShouldClearPreviouseCoffeeCount()
+    {
+        // Arrange
+        var items = new[]
+        {
+           new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+        };
+
+        // Act
+        _machineDataProcessor.ProcessItems(items);
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeeCountStore.SavedItems.Count);
+        foreach (var item in _coffeeCountStore.SavedItems)
+        {
+            Assert.Equal("Cappuccino", item.CoffeeType);
+            Assert.Equal(1, item.Count);
+        }
+    }
+
+    public void Dispose()
+    {
+        // This runs after every test
+        // Example of cleanup here would be to remove a file we created during testing
+    }
+}
+
+// We are creating a fake interface implementation
+public class FakeCoffeeCountStore : ICoffeeCountStore
+{
+    public List<CoffeeCountItem> SavedItems { get; } = new();
+    public void Save(CoffeeCountItem item)
+    {
+        SavedItems.Add(item);
+    }
+}
